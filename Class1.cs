5fdaa56249aecd237cf061f94e1aab446e0960ab
albertoPerez25 <164548339+albertoPerez25@users.noreply.github.com>
2025-02-06 22:17:00 +0100

    Version 2.0
    - Updated to use ScriptHookDotNetV3
    - Added phone menu using iFruitAddonv2
    - Added more configurations to .ini

--- Class1.cs
+++ Class1.cs
@@ -2,52 +2,85 @@
 using System.Windows.Forms;
 using GTA;
 using GTA.Native;
-using GTA.Math;
+using GTA.UI;
+using iFruitAddon2;
 
 
 namespace ScriptGTAV_1
 {
     public class Main : Script
     {
-        Vehicle car;
-        private ScriptSettings config; 
+        private ScriptSettings config;
+        Boolean use_key;
         Keys my_key;
         Boolean loadedMP;
+        Boolean use_phone;
+        readonly CustomiFruit _iFruit;
+        iFruitContact contactA;
         public Main()
         {
-            Tick += onTick;
-            KeyDown += onKeyDown;
-            config = ScriptSettings.Load("scripts\\MP_MapSwitch.ini"); 
-            my_key = config.GetValue<Keys>("Key", "my_key", Keys.K);
-            loadedMP = config.GetValue<Boolean>("Bool", "MP_loaded", false);
-        }
-        private void onTick(object sender, EventArgs e)
-        {
+            Tick += OnTick;
 
+            // Custom phone creation
+            _iFruit = new CustomiFruit();
+
+            KeyDown += onKeyDown;
+            config = ScriptSettings.Load("scripts\\MP_MapSwitch.ini");
+            loadedMP = config.GetValue<Boolean>("Bool", "MP_loaded", false);
+
+            use_key = config.GetValue<Boolean>("Key", "use_key", false);
+            if (use_key)
+                my_key = config.GetValue<Keys>("Key", "my_key", Keys.K);
+
+            use_phone = config.GetValue<Boolean>("Phone_contact", "use_phone", true);
+            if (!use_key || use_phone) 
+            {
+                // New contact (no wait before picking up the phone)
+                contactA = new iFruitContact("Switch MP map")
+                {
+                    DialTimeout = 1,            // Delay before answering
+                    Active = true,              // true = the contact is available and will answer the phone
+                    Icon = ContactIcon.Facebook // Contact's icon
+                };
+                contactA.Name = "Switch MP map";
+                contactA.Answered += new ContactAnsweredEvent(switchMap);
+                _iFruit.Contacts.Add(contactA); // Add the contact to the phone
+            } 
+        }
+
+        void OnTick(object sender, EventArgs e)
+        {
+            if (use_phone)
+                _iFruit.Update();
         }
         private void onKeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == my_key)
+            if (use_key && e.KeyCode == my_key)
             {
-                Function.Call(Hash._0x9BAE5AD2508DF078, true); // SET_INSTANCE_PRIORITY_MODE(true);
-                Script.Wait(500);   // Crash fix
-                Function.Call(Hash._0xE37B76C387BE28ED);       // REMOVE_IPL()
-
-                if (!loadedMP)
-                {
-                    //Load MP map:
-                    UI.Notify("Loading MP map...");
-                    Function.Call(Hash._0x0888C3502DBBEEF5);   // ON_ENTER_MP();
-                    loadedMP = !loadedMP;
-                }
-                else 
-                {
-                    //Load SP map:
-                    UI.Notify("Loading SP map...");
-                    Function.Call(Hash._0xD7C10C4A637992C9);   // ON_ENTER_SP();
-                    loadedMP = !loadedMP;
-                }
+                switchMap(contactA);
             }
+        }
+
+        private void switchMap(iFruitContact contactA) 
+        {
+            _iFruit.Close();
+            Function.Call((Hash)0x9BAE5AD2508DF078, true); // SET_INSTANCE_PRIORITY_MODE(true);
+            Script.Wait(500);   // Crash fix
+            Function.Call((Hash)0xE37B76C387BE28ED);       // REMOVE_IPL()
+
+            if (!loadedMP)
+            {
+                //Load MP map:
+                Notification.Show("Loading MP map...", true);
+                Function.Call((Hash)0x0888C3502DBBEEF5);   // ON_ENTER_MP();
+            }
+            else
+            {
+                //Load SP map:
+                Notification.Show("Loading SP map...", true);
+                Function.Call((Hash)0xD7C10C4A637992C9);   // ON_ENTER_SP();
+            }
+            loadedMP = !loadedMP;
         }
     }
 }
